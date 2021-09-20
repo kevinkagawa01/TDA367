c47754a92a620ffa6d810cfa5228ed3d56b0daed
Simon Holst <simonhol@student.chalmers.se>
2021-09-20 09:01:51 +0200

    linked all pages to the view main class

---
+++ src/main/java/com/cbm/tda367/ApplicationController.java
@@ -0,0 +1,9 @@
+package com.cbm.tda367;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class ApplicationController {
+
+
+}
--- src/main/java/com/cbm/tda367/SellPage.java
+++ src/main/java/com/cbm/tda367/SellPage.java
@@ -1,6 +1,22 @@
 package com.cbm.tda367;
 
+import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.AnchorPane;
 
+import java.io.IOException;
+
 public class SellPage extends AnchorPane {
+    public SellPage() {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("sell-page.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+
+        try
+        {
+            fxmlLoader.load();
+        } catch (IOException exception)
+        {
+            throw new RuntimeException(exception);
+        }
+    }
 }
