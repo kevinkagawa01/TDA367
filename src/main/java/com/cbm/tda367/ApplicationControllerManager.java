af90d466a81b2b8325588bd04ae35eaab248eeec
Simon Holst <simonhol@student.chalmers.se>
2021-09-23 15:11:41 +0200

    implemented methods to swtich page in the view manager

--- src/main/java/com/cbm/tda367/ApplicationControllerManager.java
+++ src/main/java/com/cbm/tda367/ApplicationControllerManager.java
@@ -1,12 +1,35 @@
 package com.cbm.tda367;
 
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class ApplicationControllerManager {
 
+    /* model & view */
+    private ApplicationModel model;
+    private ApplicationViewManager view;
+
+    /* controllers */
     private final LoginPageController loginPageController = new LoginPageController(this);
     private final ShopPageController shopPageController = new ShopPageController(this);
     private final SellPageController sellPageController = new SellPageController(this);
     private final AccountPageController accountPageController = new AccountPageController(this);
+
+    public ApplicationControllerManager(ApplicationModel model, ApplicationViewManager view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    void goToShopPage(){
+
+    }
+
+    void goToSellPage(){
+
+    }
+
+    void goToAccountPage(){
+
+    }
 }
--- src/main/java/com/cbm/tda367/ApplicationViewManager.java
+++ src/main/java/com/cbm/tda367/ApplicationViewManager.java
@@ -2,9 +2,12 @@ package com.cbm.tda367;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.layout.AnchorPane;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class ApplicationViewManager implements Initializable,Observer {
@@ -12,21 +15,42 @@ public class ApplicationViewManager implements Initializable,Observer {
     @FXML
     private AnchorPane mainAnchorPane;
 
+    /* views */
     private final LoginPageView loginPage = new LoginPageView();
     private final ShopPageView shopPage = new ShopPageView();
     private final SellPageView sellPage = new SellPageView();
     private final AccountPageView accountPage = new AccountPageView();
+    /*  */
+    private final List<Node> viewList = new ArrayList<>();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        mainAnchorPane.getChildren().add(loginPage);
-        mainAnchorPane.getChildren().add(shopPage);
-        mainAnchorPane.getChildren().add(sellPage);
-        mainAnchorPane.getChildren().add(accountPage);
+
+        viewList.add(accountPage);
+        viewList.add(sellPage);
+        viewList.add(loginPage);
+        viewList.add(shopPage);
+
+        for (Node view : viewList){
+            mainAnchorPane.getChildren().add(view);
+        }
+
     }
 
     @Override
     public void update() {
         //TODO: what should views update when model is updated?
+    }
+
+    void goToShopPage(){
+        mainAnchorPane.getChildren().get(viewList.indexOf(shopPage)).toFront();
+    }
+
+    void goToSellPage(){
+        mainAnchorPane.getChildren().get(viewList.indexOf(sellPage)).toFront();
+    }
+
+    void goToAccountPage(){
+        mainAnchorPane.getChildren().get(viewList.indexOf(accountPage)).toFront();
     }
 }
