d137667183c08023ef592507d8380a3408642bb4
Simon Holst <simonhol@student.chalmers.se>
2021-09-23 14:39:45 +0200

    SellPageController's scenebuilder elemens defined

--- src/main/java/com/cbm/tda367/SellPageController.java
+++ src/main/java/com/cbm/tda367/SellPageController.java
@@ -1,8 +1,26 @@
 package com.cbm.tda367;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.shape.Rectangle;
 
 public class SellPageController implements GolbalMenu{
+
+    /* global navigation buttons */
+    @FXML private ImageView shopButton;
+    @FXML private ImageView accountButton;
+
+    /* listing elements */
+    @FXML private TextField bookNumber;
+    @FXML private TextField bookPrice;
+    @FXML private TextArea bookDescription;
+    @FXML private ImageView bookImage;
+
+    /* sell page elements */
+    @FXML private Rectangle createListingButton;
 
     public SellPageController() {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("sell-page.fxml"));
