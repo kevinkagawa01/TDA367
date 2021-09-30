e6826e095250388791a5639ded6b97c641847c83
kevinkagawa01 <kevinkagawa01@gmail.com>
2021-09-30 15:06:54 +0200

    Created a BookViewController with shop-page-book.fxml connected

--- src/main/java/com/cbm/tda367/BookViewController.java
+++ src/main/java/com/cbm/tda367/BookViewController.java
@@ -1,6 +1,36 @@
 package com.cbm.tda367;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.IOException;
+
 public class BookViewController {
 
+    private ControllerManager manager;
+    private Book book;
+
+    // FXML elements
+    @FXML
+    ImageView view;
+
+    public BookViewController(Book book, ControllerManager manager) {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("shop-page-book.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+        this.book=book;
+        this.manager=manager;
+
+        //set image metod
+
+    }
 
 }
