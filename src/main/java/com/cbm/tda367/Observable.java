db0c9476f1c2f012e2a9ca364c75657e9ab0f17e
Simon Holst <simonhol@student.chalmers.se>
2021-10-05 09:59:59 +0200

    Added javaDoc

--- src/main/java/com/cbm/tda367/Observable.java
+++ src/main/java/com/cbm/tda367/Observable.java
@@ -1,5 +1,13 @@
 package com.cbm.tda367;
 
+/** Interface implemented by an observable, notifying all of its observers when desired.
+ * @author Kevin Pham
+ * @author Simon Holst
+ * @author Carl-Magnus Wall
+ * @author Pegah Amanzadeh
+ * @version 0.1
+ * @since 0.1
+ * */
 public interface Observable {
 
     void notifyObservers();
--- src/main/java/com/cbm/tda367/Observer.java
+++ src/main/java/com/cbm/tda367/Observer.java
@@ -1,5 +1,13 @@
 package com.cbm.tda367;
 
+/** Interface implemented by an observer, observing an observable implementing that corresponding interface.
+ * @author Kevin Pham
+ * @author Simon Holst
+ * @author Carl-Magnus Wall
+ * @author Pegah Amanzadeh
+ * @version 0.1
+ * @since 0.1
+ * */
 public interface Observer {
 
     void update();
