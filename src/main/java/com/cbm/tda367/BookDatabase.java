0709b2136ccf7c7464e84624cbdee770b539c819
CWTED <carlmagnuswall@gmail.com>
2021-10-05 10:14:16 +0200

    Added Javadocs

--- src/main/java/com/cbm/tda367/BookDatabase.java
+++ src/main/java/com/cbm/tda367/BookDatabase.java
@@ -7,26 +7,60 @@ import javafx.scene.image.Image;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Represents the database of Books
+ *
+ * @author Kevin Pham
+ * @author Simon Holst
+ * @author Carl-Magnus Wall
+ * @author Pegah Amanzadeh
+ * @version 1.0
+ * @since 1.0
+ */
+
 public class BookDatabase {
 
     private List<Book> bookList = new ArrayList<>();
     private static BookDatabase bookDatabase = new BookDatabase();
 
-
+    /**
+     * Constructs the Book database singleton.
+     */
     private BookDatabase() {
+
         Book korv = new Book("mårtan", "123", "0", 0, 0, "src/main/resources/Library/bookSample.png", "Fiction");
        //Kan ej hitt url samt har problem med att ENUM inparametern
+
+        // Book korv = new Book("mårtan", "123", "0", 0, 0, new Image("src/main/resources/Library/bookSample.png"), "Fiction");
+        //Kan ej hitt url samt har problem med att ENUM inparametern
+
     }
 
+    /**
+     * Retrieves the Book database singleton.
+     *
+     * @return The BookDatabase instance.
+     */
     public static BookDatabase getInstance() {
         return bookDatabase;
 
     }
 
+    /**
+     * Adds a Book to the list of Books.
+     *
+     * @param book A Book.
+     */
     public void addBook(Book book) {
         bookList.add(book);
     }
 
+    /**
+     * Retrieves the Book corresponding to a specific Book code (ISBN)
+     *
+     * @param bookCode A Book code (ISBN), as an integer.
+     * @return A Book corresponding to a specific Book code (ISBN).
+     */
     //TODO: refactor method
     public Book returnBookWithCorrespondingCode(String bookCode) {
         for (Book book : bookList) {
@@ -38,10 +72,20 @@ public class BookDatabase {
         return null;
     }
 
+    /**
+     * Removes a Book from the list of Books.
+     *
+     * @param book A Book.
+     */
     public void removeBook(Book book) {
         bookList.remove(book);
     }
 
+    /**
+     * Retrieves the list of Books.
+     *
+     * @return An ArrayList of Books.
+     */
     public List<Book> getBookList() {
         return new ArrayList<>(bookList);
     }
--- src/main/java/com/cbm/tda367/ReservedBooksMiniatureViewController.java
+++ src/main/java/com/cbm/tda367/ReservedBooksMiniatureViewController.java
@@ -4,10 +4,27 @@ import javafx.fxml.FXMLLoader;
 
 import java.io.IOException;
 
+
 public class ReservedBooksMiniatureViewController {
+
+    /**
+     * Visual representation of the miniature view of a User's reserved books.
+     *
+     * @author Kevin Pham
+     * @author Simon Holst
+     * @author Carl-Magnus Wall
+     * @author Pegah Amanzadeh
+     * @version 1.0
+     * @since 1.0
+     */
+
 
     private ControllerManager manager;
 
+    /**
+     * Constructs the view and defines its controller.
+     * @param manager A ControllerManager. Handles all controllers.
+     */
     public ReservedBooksMiniatureViewController(ControllerManager manager) {
         this.manager = manager;
 
