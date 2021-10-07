29eb7114904e5be6e00d07bade965a0c0420712d
kevinkagawa01 <kevinkagawa01@gmail.com>
2021-10-07 14:49:58 +0200

    Fixed rating method

--- src/main/java/com/cbm/tda367/ApplicationModel.java
+++ src/main/java/com/cbm/tda367/ApplicationModel.java
@@ -72,6 +72,44 @@ public class ApplicationModel implements Observable {
         notifyObservers();
     }
 
+    public void setRatingPicture() {
+
+        double rating = currentlyLoggedInUser.getRating() ;
+        String sourcePathStar;
+        if ((int)rating == 0) {
+            sourcePathStar = "/Library/0stars.png";
+        }
+        if (rating > 0 || rating < 1) {
+            sourcePathStar = "/Library/0-5stars.png";
+        }
+        if ((int)rating == 1) {
+            sourcePathStar = "/Library/1-stars.png";
+        }
+        if (rating > 1 || rating < 2) {
+            sourcePathStar = "/Library/1-5stars.png";
+        }
+        if ((int)rating==2) {
+            sourcePathStar = "/Library/2-stars.png";
+        }
+        if (rating > 2 || rating < 3) {
+            sourcePathStar = "/Library/2-5stars.png";
+        }
+        if ((int)rating == 3) {
+            sourcePathStar = "/Library/3-stars.png";
+        }
+        if (rating > 3.5 || rating < 4) {
+            sourcePathStar = "/Library/3-5stars.png";
+        }
+        if ((int)rating == 4) {
+            sourcePathStar = "/Library/4-stars.png";
+        }
+        if (rating > 4 || rating < 5) {
+            sourcePathStar = "/Library/4-5stars.png";
+        }
+        if ((int)rating == 5) {
+            sourcePathStar = "/Library/5-stars.png";
+        }
+    }
 
 
     /*private List<Book> updateSearchResult(){
