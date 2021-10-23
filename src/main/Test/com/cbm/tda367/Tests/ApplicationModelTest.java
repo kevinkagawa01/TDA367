fb97467178df662642fac7c7923aba5742da3a63
kevinkagawa01 <kevinkagawa01@gmail.com>
2021-10-23 09:39:34 +0200

    TESTS

--- src/main/Test/com/cbm/tda367/Tests/ApplicationModelTest.java
+++ src/main/Test/com/cbm/tda367/Tests/ApplicationModelTest.java
@@ -1,30 +1,55 @@
 package com.cbm.tda367.Tests;
 
+import com.cbm.tda367.model.ApplicationModel;
+import com.cbm.tda367.model.Listing;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ApplicationModelTest {
 
+    ApplicationModel model;
+
+    @BeforeEach
+    public void testSetUp() {
+        model = ApplicationModel.getInstance();
+
+
+    }
 
     @Test
     void notifyObservers() {
+
+
     }
 
     @Test
     void addObserver() {
+
     }
 
     @Test
     void getListingDatabase() {
+        assertEquals(model.getListingDatabase().size(), 6);
     }
 
     @Test
     void editListing() {
+
+        model.editListing("TMA660","new","299","nice",1);
+        List<Listing> listingDatabase = model.getListingDatabase();
+        assertEquals(listingDatabase.get(0).getPrice(),"299");
     }
 
     @Test
     void addListing() {
+
+        model.addListing("TMA660","new","299","nice");
+        List<Listing> listingDatabase = model.getListingDatabase();
+        assertEquals(listingDatabase.size(),7);
     }
 
     @Test
@@ -45,15 +70,20 @@ class ApplicationModelTest {
 
     @Test
     void reserveListing() {
+        List<Listing> listingDatabase = model.getListingDatabase();
+        Listing listTest=listingDatabase.get(2);
+        model.reserveListing(listTest);
+        assertEquals(model.getCurrentlyLoggedInUser().getReservedListings(),1);
     }
 
     @Test
     void removeBookFromReservedList() {
     }
-
+           //***************************************************************************//
     @Test
     void purchaseDone() {
     }
+
 
     @Test
     void removePurchaseListingBooks() {
